7dd574a16869e2d3160b2513ef2a4e6919c71021
Mika <mikadewitz@gmail.com>
2015-01-22 19:41:37 -1000

    generate passes tests

--- generator.js
+++ generator.js
@@ -1,5 +1,12 @@
 module.exports = {
-  generate : function (){
-    
+  generate : function(n) {
+    var result = [];
+    var maxNumPlusOne = 1001;
+      for (var i = 0; i < n; i++){
+      result.push(Math.floor(Math.random()*maxNumPlusOne));
+      }
+      return result;
   }
 };
+
+
--- spec/generator-spec.js
+++ spec/generator-spec.js
@@ -1,17 +1,18 @@
-var gen = require('../generator');
+var gen = require('../generator.js');
 
 describe("generate", function() {
 
-  it("Must have a function named generate()", function(){
+  it("Must have a function named generate()", function() {
     expect(gen.generate).toBeDefined();
+    expect(gen.generate).toEqual(jasmine.any(Function));
   });
 
   it("should return an array of 'n' amount of randomnly generated numbers", function(){
-    var array = gen.generate(1000);
-    expect(array.length).toEqual(1000);
+    var array1 = gen.generate(1000);
+    expect(array1.length).toEqual(1000);
     var array2 = gen.generate(1000);
     expect(array2.length).toEqual(1000);
-    expect(array).not.toEqual(array2);
+    expect(array1).not.toEqual(array2);
 
   });
 });
