d5e39aee476e8e5209fa0a8eb320b8f24c91cc7a
Mika <mikadewitz@gmail.com>
2015-01-23 11:49:47 -1000

    generate function returns only unique values in array

--- generator.js
+++ generator.js
@@ -1,11 +1,18 @@
 module.exports = {
   generate : function(n) {
-    var result = [];
-    var maxNumPlusOne = 1001;
-      for (var i = 0; i < n; i++){
-      result.push(Math.floor(Math.random()*maxNumPlusOne));
+    var maxNumPlusOne = n + 1;
+    var uniq = [];
+
+    for (var i = 0; i < n; i++){
+      var randoNum = Math.floor(Math.random()*maxNumPlusOne);
+      
+      while (uniq.indexOf(randoNum) !== -1) {
+        randoNum = Math.floor(Math.random()*maxNumPlusOne); 
+      
       }
-      return result;
+      uniq.push(randoNum);
+    }  
+    return uniq;
   }
 };
 
--- spec/generator-spec.js
+++ spec/generator-spec.js
@@ -12,7 +12,18 @@ describe("generate", function() {
     expect(array1.length).toEqual(1000);
     var array2 = gen.generate(1000);
     expect(array2.length).toEqual(1000);
-    expect(array1).not.toEqual(array2);
+    
+    var uniq = {};
+    for (var i=0; i < array1.length; i++) {
+      uniq[array1[i]] = true;
+    }
+    expect(Object.keys(uniq).length).toEqual(array1.length);
+    
+    var uniq2 = {};
+    for (var j=0; j < array2.length; j++) {
+      uniq2[array2[j]] = true;
+    }
+    expect(Object.keys(uniq2).length).toEqual(array2.length);
 
   });
 });
